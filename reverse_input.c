350ae7b249f9a991d61d31bfdedee0359fc0367f
rohan37-pro <rohan2002maji@gmail.com>
2021-11-26 10:44:00 -0500

    reverse the input given by user

---
+++ reverse_input.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main()
+{
+	char inp[100];
+	int i, len ;
+	printf("enter digit : ");
+	scanf("%s",&inp);
+	len = strlen(inp);
+	len -= 1;
+	for (i=len ; i>=0; --i){
+		printf("%c",inp[i]);
+	}
+	printf("\n");
+}
